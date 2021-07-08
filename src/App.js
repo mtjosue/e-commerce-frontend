2acc6d7aac22b8c1b2a54ca31d89b960af8e6fcf
Josue Mata <mtjosue@gmail.com>
2021-07-08 17:10:25 -0400

    Lesson on scss and creating the homepage, directory, menu-item of the project.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import logo from './logo.svg';
-// import "./App.css";
-import HomePage from "./components/home-page/homepage.component";
+import "./App.css";
+import HomePage from "./pages/home-page/homepage.component";
 
 function App() {
   return (
